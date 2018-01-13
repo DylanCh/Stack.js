a6761d8c1aa72cc506d63df330e96c00bfa1c45b
Dylan Chen <hchennyc@outlook.com>
2018-01-12 21:32:22 -0500

    init

---
+++ Stack.js
@@ -0,0 +1,35 @@
+var Stack =  function(type){
+    if(type===null || type===undefined){
+        throw new SyntaxError('Please specify a type');
+    }  
+    else if (typeof(type)!=='string'){
+        throw new TypeError('Please provide a type as a string (Surrounded by quotation marks).');
+    }
+    else {
+        this.type = type;
+        this.core = [];   
+    }
+};
+
+Stack.prototype.push = function(val){
+    if(typeof(val)!==this.type){
+        throw new TypeError(`Value pushed to the stack must by of ${typeof(this.type)} type.`);
+    }
+    else this.core.push(val);
+};
+
+Stack.prototype.pop = function(){
+    return this.core.pop();
+};
+
+Stack.prototype.toArray = function(){
+    return this.core;
+};
+
+Stack.prototype.foreach = function(){
+    return this.core.forEach;
+};
+
+Stack.prototype.peek = function(){
+    return this.core[this.core.length-1];
+}
